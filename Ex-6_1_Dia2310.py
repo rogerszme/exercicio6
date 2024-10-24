c2901791095090e07afb88761774496895f97559
Rogério Gomes <rogerszme@hotmail.com>
2024-10-24 09:34:05 -0300

    Exercícios 6 Numero 1 e 2
    Exercícios 6 Numero 1 e 2

---
+++ Ex-6_1_Dia2310.py
@@ -0,0 +1,6 @@
+nome = input('Digite o Nome: ')
+sobrenome = input('Digite o Sobrenome: ')
+idade = input('Digite o idade: ')
+lista = [nome, sobrenome, idade]
+
+print('Meu Nome é '+ lista[0].title()+' '+lista[1].title()+' e tenho '+ lista[2].title()+' Anos.') 
---
+++ Ex-6_2_Dia2310.py
@@ -0,0 +1,28 @@
+numero = int(input('Digite um Número de 1 a 12: '))
+
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+if numero == 1:
+    print(meses[0])
+elif numero == 2:
+    print(meses[1])
+elif numero == 3:
+    print(meses[2])
+elif numero == 4:
+    print(meses[3])
+elif numero == 5:
+    print(meses[4])
+elif numero == 6:
+    print(meses[5])
+elif numero == 7:
+    print(meses[6])
+elif numero == 8:
+    print(meses[7])
+elif numero == 9:
+    print(meses[8])
+elif numero == 10:
+    print(meses[9])
+elif numero == 11:
+    print(meses[10])
+elif numero == 12:
+    print(meses[11])
